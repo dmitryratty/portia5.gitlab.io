faf971777ac99e133fd4f23976b3fca8861d6047
Dmitry Ratty <dmitryratty@gmail.com>
2024-08-21 05:32:51 +0300

    Favicon generation only on source modification

--- src/main/kotlin/Favicon.kt
+++ src/main/kotlin/Favicon.kt
@@ -1,6 +1,9 @@
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.nio.file.Path
 import kotlin.io.path.absolutePathString
-import kotlin.io.path.pathString
+import kotlin.io.path.getLastModifiedTime
 
 /**
  * https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
@@ -8,10 +11,19 @@ import kotlin.io.path.pathString
  */
 class Favicon {
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
+        @JvmStatic
+        fun main(args: Array<String>) {
             Favicon().main()
         }
     }
+
+    @Serializable
+    data class BuildInfo(val lastModifiedSvg: Long, val lastModifiedKt: Long)
+
+    val ktSrc = Utils.projectDir.resolve("src/main/kotlin/Favicon.kt")
+    val otherPath = Utils.resourcesDir.resolve("other")
+    val svgSrc = otherPath.resolve("favicon.svg")
+    val jsonFile = Utils.resourcesDir.resolve("favicon.json").toFile()
 
     private fun svgToPng(src: Path, dst: Path, size: Int) {
         val output = rattyExec(
@@ -27,17 +39,30 @@ class Favicon {
         check(dst.toFile().exists())
     }
 
+    private fun generationRequired(): Boolean {
+        if (!jsonFile.exists()) return true
+        val buildInfo = Json.decodeFromString<BuildInfo>(jsonFile.readText())
+        if (svgSrc.getLastModifiedTime().toMillis() > buildInfo.lastModifiedSvg) return true
+        if (ktSrc.getLastModifiedTime().toMillis() > buildInfo.lastModifiedKt) return true
+        return false
+    }
+
+    private fun generationCompleted() {
+        val format = Json { prettyPrint = true }
+        val buildInfo = BuildInfo(
+            svgSrc.getLastModifiedTime().toMillis(), ktSrc.getLastModifiedTime().toMillis()
+        )
+        jsonFile.writeText(format.encodeToString(buildInfo))
+    }
+
     fun main() {
-        val projectPath = Utils().projectDir
-        val otherPath = Utils().resourcesDir.resolve("other")
-        val svgSrc = otherPath.resolve("favicon.svg")
+        if (!generationRequired()) return
+        println("Generating favicon.")
         val tmpPng = otherPath.resolve("favicon-temp.png")
         val icoDst = otherPath.resolve("favicon.ico")
         svgToPng(svgSrc, tmpPng, 32)
         val output = rattyExec(
-            "convert",
-            tmpPng.absolutePathString(),
-            icoDst.absolutePathString()
+            "convert", tmpPng.absolutePathString(), icoDst.absolutePathString()
         )
         if (output.isNotEmpty()) {
             println(output)
@@ -46,5 +71,6 @@ class Favicon {
         svgToPng(svgSrc, otherPath.resolve("icon-512.png"), 512)
         svgToPng(svgSrc, otherPath.resolve("icon-192.png"), 192)
         svgToPng(svgSrc, otherPath.resolve("apple-touch-icon.png"), 180)
+        generationCompleted()
     }
 }
