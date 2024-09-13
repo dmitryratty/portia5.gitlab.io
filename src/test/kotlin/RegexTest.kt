acf0c0c6570beb1154ae24895189938e5735dda2
Dmitry Ratty <dmitryratty@gmail.com>
2024-09-13 10:20:34 +0300

    manifest.webmanifest shortcuts

---
+++ src/test/kotlin/RegexTest.kt
@@ -0,0 +1,56 @@
+class RegexTest {
+    /*
+    TODO
+
+    "(.+)(/build/intermediates/cxx/)(.+)(/.{8}/)(logs|obj/local)(.+)"
+    "\$1\$2\$3/<hash>/\$5\$6"
+    
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/logs/arm64-v8a
+    /path/to/module/build/intermediates/cxx/Release/<hash>/logs/arm64-v8a
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/obj/local/arm64-v8a
+    /path/to/module/build/intermediates/cxx/Release/<hash>/obj/local/arm64-v8a
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/logs/armeabi-v7a
+    /path/to/module/build/intermediates/cxx/Release/<hash>/logs/armeabi-v7a
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/obj/local/armeabi-v7a
+    /path/to/module/build/intermediates/cxx/Release/<hash>/obj/local/armeabi-v7a
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/logs/x86
+    /path/to/module/build/intermediates/cxx/Release/<hash>/logs/x86
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/obj/local/x86
+    /path/to/module/build/intermediates/cxx/Release/<hash>/obj/local/x86
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/logs/x86_64
+    /path/to/module/build/intermediates/cxx/Release/<hash>/logs/x86_64
+
+    /path/to/module/build/intermediates/cxx/Release/5h6q5c2l/obj/local/x86_64
+    /path/to/module/build/intermediates/cxx/Release/<hash>/obj/local/x86_64
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/logs/arm64-v8a
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/logs/arm64-v8a
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/obj/local/arm64-v8a
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/obj/local/arm64-v8a
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/logs/armeabi-v7a
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/logs/armeabi-v7a
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/obj/local/armeabi-v7a
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/obj/local/armeabi-v7a
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/logs/x86
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/logs/x86
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/obj/local/x86
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/obj/local/x86
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/logs/x86_64
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/logs/x86_64
+
+    /path/to/module/build/intermediates/cxx/Debug/3o5x166y/obj/local/x86_64
+    /path/to/module/build/intermediates/cxx/Debug/<hash>/obj/local/x86_64
+    */
+}
