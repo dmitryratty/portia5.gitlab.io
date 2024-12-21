14ba6c18c8e4717f8a9f88ca7bce9836af6492df
Portia <dmitryratty@gmail.com>
2024-12-21 14:55:13 +0300

    Truck-kun

--- public/src-main-kotlin/GalleryGrid.kt
+++ public/src-main-kotlin/GalleryGrid.kt
@@ -16,17 +16,26 @@ class GalleryGrid {
     fun main() {
         val imagesInfosFile = srcRawDir.resolve("image/gallery/infos.json").toFile()
         val imagesInfos = Json.decodeFromString<List<ImageInfo>>(imagesInfosFile.readText())
+        val displaySizeHorizontal = 300
+        val displaySizeSquare = 340
         val builder = StringBuilder()
-        val displaySize = 320
+        builder.append("""<div class="justified-image-grid">""")
         imagesInfos.forEach {
             if (builder.isNotEmpty()) builder.appendLine()
             val imgElem = """<img src="/image/gallery/${it.name}" alt="${it.name}"/>"""
             val w = it.w.toDouble()
             val h = it.h.toDouble()
+            val displaySize = if (w > h) displaySizeHorizontal else displaySizeSquare
             val iElem = """<i style="padding-bottom:${h/w*100}%"></i>"""
             val divElem = """<div style="flex-grow:${w*100/h};flex-basis:${w*displaySize/h}px;">$iElem$imgElem</div>"""
             builder.append(divElem)
         }
+        for (i in 1..10) {
+            if (builder.isNotEmpty()) builder.appendLine()
+            builder.append("""<div class="placeholder"></div>""")
+        }
+        builder.appendLine()
+        builder.append("""</div>""")
         val template = srcRawDir.resolve("test/image/gallery-template.html").toFile()
         srcRawDir.resolve("test/image/test4.html").toFile().writeText(
             template.readText().replace("<!-- IMAGES -->", builder.toString()))
