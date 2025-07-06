8d4036afcdd7ee5bc056458be93ab22c934410ea
Portia <dmitryratty@gmail.com>
2025-07-06 06:06:30 +0300

    <<< * * * >>>

--- public/src-main-kotlin/Constants.kt
+++ public/src-main-kotlin/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val includeParag = "paragraph"
     const val includeSection = "section"
     const val includeTag = "#include "
-    const val abstractSeparatorTemp = "<< * * * >>"
+    const val abstractSeparatorTemp = "<<< * * * >>>"
     const val abstractSeparator = "\n\n$abstractSeparatorTemp\n\n"
     const val sectionSeparator = "\n\n* * *\n\n"
     const val paragSeparator = "\n\n"
